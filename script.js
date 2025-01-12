f8400777e0b9f61ef9cbc0f039eda24c22826a9e
vodogrei13 <vodogrei15@mail.ru>
2025-01-12 21:40:55 +0300

    debag and fix

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let header = document.querySelector('.js_header');
-let headerH = document.querySelector('.js_header').clientHeight;
+let header = document.querySelector(".js_header");
+let headerH = document.querySelector(".js_header").clientHeight;
 
 document.onscroll = function () {
-    let scroll = window.scrollY;
+  let scroll = window.scrollY;
 
-    if(scroll > headerH) {
-        header.classList.add('fixed')
-        document.body.style.paddingTop = headerH + 'px';
-    } else {
-        header.classList.remove('fixed');
-        document.body.removeAttribute('style');
-    }
-}
+  if (scroll > headerH) {
+    header.classList.add("fixed");
+    document.body.style.paddingTop = headerH + "px";
+  } else {
+    header.classList.remove("fixed");
+    document.body.removeAttribute("style");
+  }
+};
